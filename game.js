1053e517ff43700a197c6df06aaa940cce8c065e
Anthony Delorie <antonio@Anthonys-MacBook-Pro.local>
2017-03-18 09:16:33 -0400

    added opponents vs one AI... added one buildSnake function same baseline, and assign to either opponents[] or PLAYER

--- game.js
+++ game.js
@@ -1,163 +1,199 @@
 //GLOBAL CONSTANTS
-var SNAKE_IMAGES = ['ball','ball2','ball4','ball5','ball6','ball7','ball8','ball9','ball10','ball11', 'ball12','ball13','ball15','ball16','ball17','ball18','ball19','ball20','ball21','ball22','ball23']
-var EYE = 'eyeball2'
-var EYE_SIZE = 20
-var FOOD_IMG = 'candy2'
-var FOOD_SIZE = 40
-var DISTANCE = 20
-var START_GAME = 'TAP TO START'
-var MSG = text(START_GAME,'lime',CENTER)
+var SNAKE_IMAGES = ['ball','ball2','ball4','ball5','ball6','ball7','ball8','ball9','ball10','ball11', 'ball12','ball13','ball15','ball16','ball17','ball18','ball19','ball20','ball21','ball22','ball23'];
+var EYE = 'eyeball2';
+var EYE_SIZE = 20;
+var CANDY = 'candy2';
+var EXPIRED_FOOD = 'candy4';
+var FOOD_SIZE = 40;
+var DISTANCE = 20;
+var centerX = this.width()/2, centerY = this.height()/2;
+var MSG1 = text('TAP TO SPAWN',centerX, centerY - 22,'lime',CENTER);
+var MSG2 = text('DRAG TO MOVE',centerX, centerY + 22,'junglegreen',CENTER);
 //GLOBAL VARIABLES
-var PLAYER = {
-  SIZE:75,
-  SPEED: 15,
-  SCORE: text('0', 737, 1015,40, 'lime',RIGHT)
-}
-var AI = {
-  SIZE:75,
-  SPEED: 15,
-  SCORE: text('0', 30, 1015,40, 'pink',LEFT)
-}
-var n = 5, y, food
-var start = init
-fill('space4')
+var PLAYER;
+var opponents = [];
+var n = 5;
+var food = stamp(CANDY,0,0,2);
 
-function buildSnake(SNAKE,x,y) {
-  SNAKE.consumed = 0
-  SNAKE.SCORE.change('')
-  SNAKE.head = stamp(SNAKE.IMG,x,y, SNAKE.SIZE)
-  SNAKE.LEFT_EYE = stamp(EYE,SNAKE.head.x-10,SNAKE.head.y-20,EYE_SIZE)
-  SNAKE.RIGHT_EYE = stamp(EYE,SNAKE.head.x+10,SNAKE.head.y-20,EYE_SIZE)
-  SNAKE.body = []
-  while (SNAKE.body.length < n) {
-    y = SNAKE.head.y - ((n- SNAKE.body.length) * DISTANCE)
-    SNAKE.body.push(stamp(SNAKE.IMG,SNAKE.head.x,y,SNAKE.SIZE))
-  }
-  SNAKE.body.push(SNAKE.head);
-  SNAKE.head.front()
+fill('space4');
+
+function buildSnake() {
+    var SNAKE = {
+        IMG: random(SNAKE_IMAGES),
+        consumed: 0,
+        SIZE:60,
+        SPEED: 15
+    };
+    var x = random([random(-20,-100),random(800,900)]);
+    var y = random([random(-20,-100),random(1070,1150)]);
+    var tempY;
+    SNAKE.head = stamp(SNAKE.IMG,x,y, SNAKE.SIZE);
+    SNAKE.LEFT_EYE = stamp(EYE,SNAKE.head.x-10,SNAKE.head.y,EYE_SIZE);
+    SNAKE.RIGHT_EYE = stamp(EYE,SNAKE.head.x+10,SNAKE.head.y,EYE_SIZE);
+    SNAKE.body = [];
+    while (SNAKE.body.length < n) {
+        tempY = SNAKE.head.y + ((n- SNAKE.body.length) * DISTANCE);
+        SNAKE.body.push(stamp(SNAKE.IMG,SNAKE.head.x,tempY,SNAKE.SIZE));
+    }
+    SNAKE.head.front();
+    SNAKE.head.aim(400,500);
+    return SNAKE
 }
-function buildPlayer() {
-  buildSnake(PLAYER,400,400)
-}
+
 function hideEyes(SNAKE) {
-  if (SNAKE.RIGHT_EYE) SNAKE.RIGHT_EYE.hide()
-  if (SNAKE.LEFT_EYE) SNAKE.LEFT_EYE.hide()
+    if (SNAKE.RIGHT_EYE) SNAKE.RIGHT_EYE.hide();
+    if (SNAKE.LEFT_EYE) SNAKE.LEFT_EYE.hide();
 }
+
 function buildAI() {
-  hideEyes(AI)
-  if (AI.body && AI.body.length) AI.body.forEach(function(s){ s.hide() })
-  var randomX = random([random(-20,-100),random(800,900)])
-  var randomY = random([random(-20,-100),random(1070,1150)])
-  buildSnake(AI,randomX,randomY)
+    opponents.push(buildSnake());
 }
+
 function setStage(){
-  song('bumblebee')
-  MSG.change('')
+    song('bumblebee')
+    MSG1.hide();
+    MSG2.hide();
 }
+
+function notifyLaunch () {
+    var x = PLAYER.head.x+0;
+    var y = PLAYER.head.y+0;
+    x = x < 0 ? x + 200 : x > 750 ? x - 200 : x
+    y = y < 0 ? y + 200 : y > 1050 ? y - 200 : y
+    arrow = stamp('arrow', x, y,0)
+    arrow.aim(PLAYER.head)
+    arrow.size(150)
+    arrow.size(0,2000)
+}
+
 function setBG(){
-  PLAYER.IMG = random(SNAKE_IMAGES)
-  AI.IMG = random(SNAKE_IMAGES.filter(function(s){return s!==PLAYER.IMG}))
-  fill([PLAYER.IMG,AI.IMG].includes('ball8') ? 'park' : 'space4');
+    fill([PLAYER.IMG].includes('ball8') ? 'park' : 'space4');
 }
+
 function gameLoop () {
-  var collisions
-  if (!food) { spawnFood() }
-  aimHead(AI,food.x,food.y)
-  if (AI.body && AI.body.length) {
-      slither(AI)
-  }
-  if (PLAYER.body && PLAYER.body.length) slither(PLAYER)
-
-  collisions = PLAYER.body.filter(hitAI)
-  if (collisions.length) {
-    console.log(collisions)
-    die(AI)
-    setBG()
-    buildAI()
-  }
-  
-  collisions = AI.body.filter(hitPLAYER)
-  if (collisions.length) {
-    console.log(collisions)
-    die(PLAYER)
-    silence()
-    MSG.change(START_GAME)
-	this.touching = function () { }
-	this.tap = init
-    return
-  }
-}
-function hitPLAYER(bodyPart) {
- return (bodyPart.hits(PLAYER.LEFT_EYE) || bodyPart.hits(PLAYER.RIGHT_EYE)) && bodyPart != AI.head 
+    var temp;
+    opponents.concat(PLAYER).forEach(function(s){
+        if (s !== PLAYER) aimHead(s,food.x,food.y);
+        if (s.body && s.body.length) slither(s);
+    });
 }
 
-function hitAI(bodyPart) {
- return (bodyPart.hits(AI.LEFT_EYE) || bodyPart.hits(AI.RIGHT_EYE)) && bodyPart != AI.head 
+function deadOpponent (s) {
+    opponents = opponents.filter(function(o) { return o !== s });
+    setBG();
+    buildAI();
+}
+
+function deadPLAYER () {
+    silence();
+    MSG1.show();
+    MSG2.show();
+    this.touching = function () { };
+    this.tap = init;
+}
+function hitSNAKE(SNAKE, s) {
+//  var bodyPartHitHead = bod => bod.hits(SNAKE.head.x,SNAKE.head.y);
+    var bodyPartHitHead = bod => bod.hits(SNAKE.head);
+    return s.body.some(bodyPartHitHead);
 }
 
 function init() {
-  find(FOOD_IMG).forEach(function(f){f.hide()})
-  setStage()
-  setBG()
-  food = null
-  buildAI()
-  buildPlayer()
-  this.tap = function () { }
-  this.touching = function () { aimHead(PLAYER,x,y) }
-  this.loop = gameLoop
+    setStage();
+    spawnFood()
+    buildAI();
+    PLAYER = buildSnake()
+    setBG();
+    notifyLaunch();
+    this.tap = function () { };
+    this.touching = navigate;
+    this.loop = gameLoop;
 }
 
-tap = init
+function navigate() {
+    if (PLAYER.head.hits(x,y) || PLAYER.body.some(function(body){return body.hits(x,y) }) ) return;
+    aimHead(PLAYER,x,y);
+}
+
+this.tap = init;
+
+function rotateHead(SNAKE) {
+    var head = SNAKE.head;
+    var r = head.rotation;
+    head.size(SNAKE.SIZE);
+    head.rotate(r);
+    SNAKE.LEFT_EYE.rotate(r);
+    SNAKE.RIGHT_EYE.rotate(r);
+}
+
+function moveHead(SNAKE) {
+    SNAKE.head.move(UP,SNAKE.SPEED);
+    SNAKE.RIGHT_EYE.move(UP,SNAKE.SPEED);
+    SNAKE.LEFT_EYE.move(UP,SNAKE.SPEED);
+    SNAKE.head.front();
+    SNAKE.LEFT_EYE.front();
+    SNAKE.RIGHT_EYE.front();
+}
 
 function slither(SNAKE) {
-    var r = SNAKE.head.rotation
-	SNAKE.head = stamp(SNAKE.IMG,SNAKE.head.x,SNAKE.head.y, SNAKE.SIZE)
-    SNAKE.head.rotate(r)
-    SNAKE.LEFT_EYE.rotate(r)
-    SNAKE.RIGHT_EYE.rotate(r)
-    SNAKE.head.move(UP,SNAKE.SPEED)
-    SNAKE.RIGHT_EYE.move(UP,SNAKE.SPEED)
-    SNAKE.LEFT_EYE.move(UP,SNAKE.SPEED)
-    SNAKE.LEFT_EYE.front()
-    SNAKE.RIGHT_EYE.front()
-    SNAKE.body.push(SNAKE.head)
-    var hasEaten = false
-    var candies = find(FOOD_IMG)
-    candies.forEach(function(candy) {
-      if (SNAKE.head.hits(candy.x,candy.y)) {
-        if (candy == food) {
-          food = null
-        	SNAKE.consumed = SNAKE.consumed + 0.9
+    if(!!SNAKE.ate) SNAKE.ate = undefined
+    else {
+        var s = SNAKE.body.shift();
+        s.hide();
+    }
+    SNAKE.body.push(stamp(SNAKE.IMG,SNAKE.head.x,SNAKE.head.y, SNAKE.SIZE))
+    rotateHead(SNAKE);
+    moveHead(SNAKE);
+
+    var anyHasEaten = false;
+//    var chance = false;//random(0,1)
+    if (SNAKE.head.hits(food.x,food.y)) {
+        food.size(0,30)
+        anyHasEaten = true
+        SNAKE.ate = true
+        spawnFood()
+        SNAKE.SIZE = SNAKE.SIZE + 1;
+        SNAKE.consumed = SNAKE.consumed + 1;
+        SNAKE.SPEED = SNAKE.SPEED + .1;
+    }
+
+    opponents.concat(PLAYER).forEach(function(s) {
+        if (s === SNAKE) return;
+        if (anyHasEaten) {
+            s.SIZE = s.SIZE - 1;
+            s.SPEED = s.SPEED + .1;
         }
-        candy.hide()
-        SNAKE.consumed = SNAKE.consumed + 0.1
-        SNAKE.SCORE.change(Math.floor(SNAKE.consumed))
-        //sound('chomp')
-        hasEaten = true
-      }
+        if (hitSNAKE(SNAKE, s)) {
+            die(SNAKE)
+            if (SNAKE === PLAYER) deadPLAYER();
+            else {
+                deadOpponent()
+//                if (chance) buildAI()
+            }
+        }
+
+
     });
-    if (hasEaten) return
-  	var s = SNAKE.body.shift();
-    s.hide()
 }
 
 function die(SNAKE) {
-  hideEyes(SNAKE)
-  SNAKE.body.map(function(s){ 
-    s.size(FOOD_SIZE)
-    s.change(FOOD_IMG)
-    s.size(0,3000)
-  })
-  SNAKE.body = []
+    hideEyes(SNAKE);
+    SNAKE.body.concat(SNAKE.head).map(function(s){
+        s.size(FOOD_SIZE);
+        s.change(CANDY);
+        s.size(0,3000);
+    });
+    SNAKE.body = [];
 }
 
 function aimHead (SNAKE,x,y) {
-  SNAKE.head.aim(x,y)
+    SNAKE.head.aim(x,y);
 }
 
 function spawnFood() {
-	var x = random(10, 740)
-    var y = random(10, 1000)
-    food = stamp(FOOD_IMG,x,y,FOOD_SIZE)
-    food.dance()
+    var x = random(10, 740);
+    var y = random(10, 1000);
+    food.size(FOOD_SIZE,500);
+    food.move(x,y)
+    food.dance();
+    food.dance();
 }
